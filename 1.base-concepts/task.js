d4b8f996ae4148900cc12453619d7fc0768ac34e
Marina Verkutis <marina.corpwork@gmail.com>
2023-08-14 21:54:54 +0400

    Task 1 completed

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,31 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = Math.pow(b, 2) - 4 * a * c;
+
+  if (d === 0) {
+    arr.push(-b/(2*a));
+  } else if (d > 0) {
+    arr.push((-b + Math.sqrt(d))/(2*a));
+    arr.push((-b - Math.sqrt(d))/(2*a));
+  } else {}
+
   return arr;
 }
 
+
+function isNumber(num) {
+  return typeof num === 'number' || !isNaN(num);
+}
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  if (isNumber(percent) && isNumber(contribution) && isNumber(amount) && isNumber(countMonths)) {
+      percent = percent / 100 / 12;
+      let bodyCredit = amount - contribution;
+      let monthlyPayment = bodyCredit * (percent + (percent / (((1 + percent) ** countMonths) - 1)));
+      let totalSum = parseFloat((monthlyPayment * countMonths).toFixed(2));
+      return totalSum;
+  }
+  return false;
 }
