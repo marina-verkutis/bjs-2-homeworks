b5c752728e0679d631c5d65b56fc9e8adfdf86a4
Marina Verkutis <marina.corpwork@gmail.com>
2023-08-15 12:44:30 +0400

    Task 2.1 completed

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,5 +1,17 @@
 function getArrayParams(...arr) {
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min)
+      min = arr[i];
+    if (arr[i] > max)
+      max = arr[i];
+    sum += arr[i];
+  }
+
+  let avg = parseFloat((sum / arr.length).toFixed(2));
   return { min: min, max: max, avg: avg };
 }
 
