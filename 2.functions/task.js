5debff10ee25c342497ee8c03ec4681d22d59844
Marina Verkutis <marina.corpwork@gmail.com>
2023-08-15 13:31:48 +0400

    Task 2.2 completed

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -16,19 +16,52 @@ function getArrayParams(...arr) {
 }
 
 function summElementsWorker(...arr) {
+  if (arr.length === 0 )
+    return 0;
 
+  const initialValue = 0;
+  return arr.reduce((accumulator, currentValue) => accumulator + currentValue, initialValue);
 }
 
 function differenceMaxMinWorker(...arr) {
-
+  if (arr.length === 0)
+    return 0;
+  
+  return Math.max(...arr) - Math.min(...arr)
 }
 
 function differenceEvenOddWorker(...arr) {
+  if (arr.length === 0 )
+    return 0;
 
+  let sumEvenElement = 0;
+  let sumOddElement = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) 
+      sumEvenElement += arr[i];
+    else 
+      sumOddElement += arr[i];
+  }
+
+  return sumEvenElement - sumOddElement;
 }
 
 function averageEvenElementsWorker(...arr) {
+  if (arr.length === 0 )
+    return 0;
 
+  let sumEvenElement = 0;
+  let countEvenElement = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumEvenElement += arr[i];
+      countEvenElement++;
+    }
+  }
+  
+  return sumEvenElement / countEvenElement;
 }
 
 function makeWork (arrOfArr, func) {
