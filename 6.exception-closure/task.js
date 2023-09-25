10f2ac50e577495902a40f64b9b70e5020272de8
Marina Verkutis <marina.corpwork@gmail.com>
2023-09-25 13:04:29 +0400

    Task 6 fixed

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,65 +1,65 @@
-﻿﻿// задание 1
+﻿﻿﻿// task 1
 function parseCount(value) {
-    const parsedValue = Number.parseFloat(value)
-    if (Number.isNaN(parsedValue)) {
-      throw new Error("Невалидное значение");
-    }
-    return parsedValue;
+  const parsedValue = Number.parseFloat(value)
+  if (Number.isNaN(parsedValue)) {
+    throw new Error("Невалидное значение");
   }
-  
-  function validateCount(value) {
-    try {
-      return parseCount(value);
-    } catch (error) {
-      return error;
-    }
+  return parsedValue
+}
+
+function validateCount(value) {
+  try {
+    return parseCount(value);
+  } catch (error) {
+    return error;
   }
-  
-  // Задание 2
-  class Triangle {
-    constructor(side1, side2, side3) {
-      this.side1 = side1;
-      this.side2 = side2;
-      this.side3 = side3;
-  
-      if (!this.isValidTriangle()) {
-        throw new Error("Треугольник с такими сторонами не существует");
-      }
-    }
-  
-    isValidTriangle() {
-      return (
-        this.side1 + this.side2 > this.side3 &&
-        this.side1 + this.side3 > this.side2 &&
-        this.side2 + this.side3 > this.side1;
-      )
-    }
-  
-    get perimeter() {
-      return this.side1 + this.side2 + this.side3;
-    }
-  
-    get area() {
-      const p = this.perimeter / 2;
-      const area = Math.sqrt(
-        p * (p - this.side1) * (p - this.side2) * (p - this.side3);
-      );
-      return Number(area.toFixed(3));
+}
+
+// task 2
+class Triangle {
+  constructor(side1, side2, side3) {
+    this.side1 = side1;
+    this.side2 = side2;
+    this.side3 = side3;
+
+    if (!this.isValidTriangle()) {
+      throw new Error("Треугольник с такими сторонами не существует");
     }
   }
-  
-  function getTriangle(side1, side2, side3) {
-    try {
-      const triangle = new Triangle(side1, side2, side3);
-      return Object.freeze(triangle);
-    } catch (error) {
-      return {
-        get perimeter() {
-          return "Ошибка! Треугольник не существует";
-        },
-        get area() {
-          return "Ошибка! Треугольник не существует";
-        },
-      }
+
+  isValidTriangle() {
+    return (
+      this.side1 + this.side2 > this.side3 &&
+      this.side1 + this.side3 > this.side2 &&
+      this.side2 + this.side3 > this.side1
+    );
+  }
+
+  get perimeter() {
+    return this.side1 + this.side2 + this.side3;
+  }
+
+  get area() {
+    const p = this.perimeter / 2;
+    const area = Math.sqrt(
+      p * (p - this.side1) * (p - this.side2) * (p - this.side3)
+    );
+    return Number(area.toFixed(3));
+  }
+}
+
+function getTriangle(side1, side2, side3) {
+  try {
+    const triangle = new Triangle(side1, side2, side3);
+    return Object.freeze(triangle);
+  } catch (error) {
+    return {
+      get perimeter() {
+        return "Ошибка! Треугольник не существует";
+      },
+      get area() {
+        return "Ошибка! Треугольник не существует";
+      },
     }
   }
+}
